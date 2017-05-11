1de99f3ddf5e0bc744d940e489553bf46888c8c1
zhengyijia <zhengyijia142@qq.com>
2017-05-11 09:20:25 +0800

    setting, password and photo pages

---
+++ js/setting.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+	
+	$("#goToPassword").click(function() {
+		window.location.href = "password.html";
+	});
+
+	$("#goToPhoto").click(function() {
+		window.location.href = "photo.html";
+	});
+
+})
