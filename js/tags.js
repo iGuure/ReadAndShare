64d0f70168304d12a87b9b15b0827b9dc41f93aa
Antsecvic <418431868@qq.com>
2017-05-12 11:32:17 +0800

    tags

--- js/tags.js
+++ js/tags.js
@@ -3,5 +3,12 @@ $(document).ready(function() {
 	$(".btn").click(function() {
 		$(this).css("background-color","yellow");
 	});
+	
+	$("#search").click(function(){
+		$("#search").css("background-color","white");
+	});
 
+	$("#finishSelect").click(function(){
+		$("#finishSelect").css("background-color","gray");
+	});
 })
