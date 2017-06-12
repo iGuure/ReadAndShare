6504955dab76f1a4ec1a180817817dce00b8611f
zhangzhishuo <164275563@qq.com>
2017-06-12 22:41:09 +0800

    zhishuo

--- ReadAndShare-zzs/js/bookshelf.js
+++ ReadAndShare-zzs/js/bookshelf.js
@@ -1,9 +1,46 @@
 $("#example-navbar-collapse").collapse({
-	toggle:true
+	toggle : true
 });
 
-function collapse(){
-	$("#example-navbar-collapse").collapse("toggle"
-	
-	
+function collapse() {
+	$("#example-navbar-collapse").collapse("toggle")
+
 }
+function removeElement() {
+
+	$(this).parent().remove();
+
+}
+$(".more").children("li").click(function(){
+	
+	$("#bookTab").children("li").removeClass('active');
+})
+$("#bookManager").click(function() {
+	$(".closeTab").toggle(200);
+    $(".closeLayer").toggle(200);
+				});
+$(".closeLayer").click(removeElement);
+$(".closeTab").click(removeElement);
+$("#new").click(function() {
+	$(this).hide();
+     var newInput = $('<li><input type="text" class="form-control input" id="input"></li>')
+	 $("#bookTab").children(":last").before(newInput);
+	 $(".input").keydown(function(e) {
+				
+		if (e.keyCode == 13) {
+		var name = $(".input").val();
+		$(this).remove();
+		var newTab = $('<li><a href="" data-toggle="tab">'+
+		'</a><div class="closeTab" style="display: none" >'+
+		'<a class="glyphicon glyphicon-remove" role="menuitem" tabindex="-1"> </a>'+
+		'</div></li></li>');
+		newTab.children(":first").append(name);
+		$("#bookTab").children(":last").before(newTab);
+		$(".closeTab").click(removeElement);
+		$("#new").show();
+         }
+		
+	});
+	 
+});
+
